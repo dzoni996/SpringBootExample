4f9b755011d1e92ec7e0420ad7bf65930fdb4abd
Nikola <nikoladodov96@gmail.com>
2023-09-03 23:19:43 +0200

    Saving data to DB

---
+++ web_store/src/main/java/com/example/web_store/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.web_store.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "marian@mail.com",
+                    LocalDate.of(1996, Month.APRIL, 3),
+                    21
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex@mail.com",
+                    LocalDate.of(1994, Month.APRIL, 3),
+                    25
+            );
+
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+}
