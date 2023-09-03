f9f8bcf2555c91b6ab788063f2dc87689d4d5a00
Nikola <nikoladodov96@gmail.com>
2023-09-03 12:18:52 +0200

    First package & run enabled

--- web_store/src/main/java/com/example/web_store/WebStoreApplication.java
+++ web_store/src/main/java/com/example/web_store/WebStoreApplication.java
@@ -1,13 +1,32 @@
 package com.example.web_store;
 
+import com.example.web_store.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class WebStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebStoreApplication.class, args);
 	}
-
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+			new Student(
+					1L,
+					"Mariam",
+					"marian@mail.com",
+					LocalDate.of(1996, Month.APRIL, 3),
+					21
+			)
+		);
+	}
 }
