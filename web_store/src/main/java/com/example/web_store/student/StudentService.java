83b6e7c8c5d31057943c47808d8d5864d6fd07ae
Nikola <nikoladodov96@gmail.com>
2023-09-04 20:29:05 +0200

    Deleting student

--- web_store/src/main/java/com/example/web_store/student/StudentService.java
+++ web_store/src/main/java/com/example/web_store/student/StudentService.java
@@ -32,4 +32,12 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + studentId + " not exists!");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
